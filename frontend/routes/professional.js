7ddb031c65b7636460c3b822ce1f39549342c06b
NathanielSnow <sno20001@byui.edu>
2022-02-26 17:08:01 -0600

    update

---
+++ frontend/routes/professional.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const professionalController = require("../controllers/professional");
+
+const router = express.Router();
+
+//GET data
+router.get("/", professionalController.getData);
+
+//localhost:8080/professional/
+module.exports = router;
